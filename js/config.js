f573be98600155a483316b9dd1d50f19b4cb76be
zopomobile <547069699@qq.com>
2025-06-30 01:30:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -123,11 +123,11 @@ const API_SITES = {
     // 这些影片和背后的产业已经使数百万男女“下海”，出卖自己的身体，甚至以此为生计。
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
-    // ckzy: {
-    //     api: 'https://www.ckzy1.com',
-    //     name: 'CK资源',
-    //     adult: true
-    // },
+    ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源',
+        adult: true
+     },
     // jkun: {
     //     api: 'https://jkunzyapi.com',
     //     name: 'jkun资源',
